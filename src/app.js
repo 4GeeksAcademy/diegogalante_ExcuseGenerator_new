6d59b6c853ab0902497092de432414e3cc253792
DiegoGalantee <diegogalante07@gmail.com>
2025-04-09 18:48:55 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+function gen() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    let who = ["My dog", "My grandma", "My sister", "My cat"];
+    let action = ["ate", "peed on", "broke", "messed up",];
+    let what = ["my homework", "my phone", "my car", "my pendrive"];
+    let when = ["while I was sleeping.", "while I was at work.", "while I was playing videogames.", "while I was exercising."];
+
+    let excuse = 
+      who[Math.floor(Math.random() * who.length)]  + " " + 
+      action[Math.floor(Math.random() * action.length)]  + " " + 
+      what[Math.floor(Math.random() * what.length)] + " " + 
+      when[Math.floor(Math.random() * when.length)];
+ 
+    document.getElementById("excuse").innerHTML = excuse;
+  }
+
+  window.onload = function() {
+    
+    gen();
+  };
+
+window.gen = gen
