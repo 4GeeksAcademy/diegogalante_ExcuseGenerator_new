84fb087a8bd56aed68ceec313a3cb12561efac70
DiegoGalantee <diegogalante07@gmail.com>
2025-07-04 16:35:05 +0000

    proyecto mejorado

--- src/app.js
+++ src/app.js
@@ -1,22 +1,53 @@
+let who = ["My dog", "My grandma", "My sister", "My cat"];
+let action = ["ate", "peed on", "broke", "messed up"];
+let what = ["my homework", "my phone", "my car", "my pendrive"];
+let when = [
+  "while I was sleeping.",
+  "while I was at work.",
+  "while I was playing videogames.",
+  "while I was exercising."
+];
+
+let currentExcuse = {
+  who: "",
+  action: "",
+  what: "",
+  when: ""
+};
+
+function getRandom(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function updateDisplay() {
+  const excuseElement = document.getElementById("excuse");
+  excuseElement.style.opacity = 0;
+
+  setTimeout(() => {
+    excuseElement.innerHTML = `${currentExcuse.who} ${currentExcuse.action} ${currentExcuse.what} ${currentExcuse.when}`;
+    excuseElement.style.opacity = 1;
+  }, 200);
+}
+
 function gen() {
-  //write your code here
-    let who = ["My dog", "My grandma", "My sister", "My cat"];
-    let action = ["ate", "peed on", "broke", "messed up",];
-    let what = ["my homework", "my phone", "my car", "my pendrive"];
-    let when = ["while I was sleeping.", "while I was at work.", "while I was playing videogames.", "while I was exercising."];
+  currentExcuse.who = getRandom(who);
+  currentExcuse.action = getRandom(action);
+  currentExcuse.what = getRandom(what);
+  currentExcuse.when = getRandom(when);
+  updateDisplay();
+}
 
-    let excuse = 
-      who[Math.floor(Math.random() * who.length)]  + " " + 
-      action[Math.floor(Math.random() * action.length)]  + " " + 
-      what[Math.floor(Math.random() * what.length)] + " " + 
-      when[Math.floor(Math.random() * when.length)];
- 
-    document.getElementById("excuse").innerHTML = excuse;
-  }
+function changePart(part) {
+  if (part === "who") currentExcuse.who = getRandom(who);
+  else if (part === "action") currentExcuse.action = getRandom(action);
+  else if (part === "what") currentExcuse.what = getRandom(what);
+  else if (part === "when") currentExcuse.when = getRandom(when);
+  updateDisplay();
+}
 
-  window.onload = function() {
-    
-    gen();
-  };
+window.onload = function () {
+  gen();
+};
 
-window.gen = gen
+window.gen = gen;
+window.changePart = changePart;
